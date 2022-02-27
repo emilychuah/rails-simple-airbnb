7a41e6a23f44e7f45db59583a1a214c98194d7c5
Emily Chuah <emily_earn@live.com>
2022-02-27 22:54:51 +1100

    Added routing, controller and view for creating a flat

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -2,4 +2,23 @@ class FlatsController < ApplicationController
   def index
     @flats = Flat.all
   end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.create(flat_params)
+    redirect_to flats_path
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
 end
